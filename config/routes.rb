6591804fb93df9d895b7a58d5c37fa36a5f7c7a2
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-11-04 10:12:54 -0400

    Director Routes
    Functional routes for director. Next, need to update index and show
    pages and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
-  get("/", ":controller => Movies", :action => "index")
+
   # ------------------- Directors --------------------
   # CREATE
   # See a blank form to build a new row, e.g., http://localhost:3000/directors/new_form
-  get("/directors/new_form", ":controller => Directors", :action => "new_form")
+  get("/directors/new_form", :controller => "directors", :action => "new_form")
   # Receieve inputs and actually save new row
-  get("/directors/create_row", ":controller => Directors", :action => "create_row")
+  get("/directors/create_row", :controller => "directors", :action => "create_row")
 
   # READ
   # See all rows in a table, e.g., http://localhost:3000/directors
-  get("/directors", ":controller => Directors", :action => "index")
+  get("/directors", :controller => "directors", :action => "index")
   # See details in an individual row, e.g., http://localhost:3000/directors/4
-  get("/directors/:id", ":controller => Directors", :action => "show")
+  get("/directors/:id", :controller => "directors", :action => "show")
 
   # UPDATE
   # Build a pre-populated form to update a row
-  get("/directors/:id/edit", ":controller => Directors", :action => "edit_form")
+  get("/directors/:id/edit", :controller => "directors", :action => "edit_form")
   # Receive inputs and save the updated row
-  get("/update_director/:id", ":controller => Directors", :action => "update_row")
+  get("/update_director/:id", :controller => "directors", :action => "update_row")
 
   # DELETE
   # Delete an individual row, e.g., http://localhost:3000/delete_director/4
-  get("/delete_director/:id", ":controller => Directors", :action => "update_row")
+  get("/delete_director/:id", :controller => "directors", :action => "destroy")
 
   # ------------------- Actors --------------------
   # CREATE
