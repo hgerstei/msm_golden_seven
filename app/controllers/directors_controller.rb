46d7602e949ddc4ee73ec8a70c45eed0b1441f88
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-11-04 08:29:53 -0400

    Director Routes
    Director routes created

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,28 @@
+class DirectorsController < ApplicationController
+  # CREATE
+  def new_form
+  end
+
+  def create_row
+    redirect "TBD"
+  end
+
+  # READ
+  def index
+  end
+
+  def show
+  end
+
+  # UPDATE
+  def edit_form
+  end
+
+  def update_row
+  end
+
+  # DELETE
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,66 @@
 Rails.application.routes.draw do
-  
+  get("/", ":controller => Movies", :action => "index")
+  # ------------------- Directors --------------------
+  # CREATE
+  # See a blank form to build a new row, e.g., http://localhost:3000/directors/new_form
+  get("/directors/new_form", ":controller => Directors", :action => "new_form")
+  # Receieve inputs and actually save new row
+  get("/directors/create_row", ":controller => Directors", :action => "create_row")
+
+  # READ
+  # See all rows in a table, e.g., http://localhost:3000/directors
+  get("/directors", ":controller => Directors", :action => "index")
+  # See details in an individual row, e.g., http://localhost:3000/directors/4
+  get("/directors/:id", ":controller => Directors", :action => "show")
+
+  # UPDATE
+  # Build a pre-populated form to update a row
+  get("/update_director", ":controller => Directors", :action => "edit_form")
+  # Receive inputs and save the updated row
+  get("/update_director/:id", ":controller => Directors", :action => "update_row")
+
+  # DELETE
+  # Delete an individual row, e.g., http://localhost:3000/delete_director/4
+  get("/delete_director/:id", ":controller => Directors", :action => "update_row")
+
+  # ------------------- Actors --------------------
+  # CREATE
+  # See a blank form to build a new row, e.g., http://localhost:3000/directors/new_form
+
+  # Receieve inputs and actually save new row
+
+  # READ
+  # See all rows in a table, e.g., http://localhost:3000/directors
+
+  # See details in an individual row, e.g., http://localhost:3000/directors/4
+
+  # UPDATE
+  # Build a pre-populated form to update a row
+
+  # Receive inputs and save the updated row
+
+  # DELETE
+  # Delete an individual row, e.g., http://localhost:3000/delete_director/4
+
+
+  # ------------------- Movies --------------------
+  # CREATE
+  # See a blank form to build a new row, e.g., http://localhost:3000/directors/new_form
+
+  # Receieve inputs and actually save new row
+
+  # READ
+  # See all rows in a table, e.g., http://localhost:3000/directors
+
+  # See details in an individual row, e.g., http://localhost:3000/directors/4
+
+  # UPDATE
+  # Build a pre-populated form to update a row
+
+  # Receive inputs and save the updated row
+
+  # DELETE
+  # Delete an individual row, e.g., http://localhost:3000/delete_director/4
+
+
 end
